d5b1e93bd57c68e3f96e2ded4cb81bfa56e6ee98
Jon Wolverton <wolverton.jr@gmail.com>
2016-03-13 07:12:20 -0700

    style & add pictures to progress bar

--- components/ProgressBar.jsx
+++ components/ProgressBar.jsx
@@ -1,16 +1,21 @@
 var React = require('react');
+var people = require('../data/people')
 
 var ProgressBar = React.createClass({
   render: function () {
     var scores = this.props.scores
     return (
       <figure className="progress-bar clearfix">
-        {
-          Object.keys(scores).map(key => {
-            var className = `progress-square score-${scores[key]}`
-            return <div className={className} key={key}></div>
-          })
-        }
+        <div className="container">
+          {
+            Object.keys(scores).map(key => {
+              var className = `progress-square score-${scores[key]}`
+              return (<div className={className} key={key}>
+                <img src={people[key].imageUrl}></img>
+              </div>)
+            })
+          }
+        </div>
       </figure>
     )
   }
